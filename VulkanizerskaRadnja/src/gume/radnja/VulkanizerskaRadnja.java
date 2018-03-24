89cd451cdd4f7ad41407e6f2bb4c5aa7541e5398
MilosBrkic <brkicmilos97@gmail.com>
2018-03-24 14:51:35 +0100

    Napisana je dokumentacije klase VulkanizerskaRadnja.
    Dokumentacija opisuje klasu, atribut i metode.

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -3,10 +3,23 @@ package gume.radnja;
 import java.util.LinkedList;
 import gume.AutoGuma;
 
+
+/**
+ * Klasa VulkanizerskaRadnja koja ima listu guma.
+ * @author Milos Brkic
+ * @version 0.1
+ */
 public class VulkanizerskaRadnja {
 	
+	/** Lista guma. */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	
+	/**
+	 * U listu guma se dodaje guma koje se prosledjuje kao parametar.
+	 * @param a guma koje treba da se doda u listu.
+	 * @throws java.lang.RuntimeException ako je uneta guma null ili vec postoji u listi.
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -16,6 +29,12 @@ public class VulkanizerskaRadnja {
 		gume.addFirst(a);
 	}
 
+	/**
+	 * Pronalazi i vraca sve gume u listi koje imaju marku i model koje se unose preko parametra.
+	 * @param markaModel Marka i model guma koje se pretrazuju.
+	 * @return listu guma kao LinkedList.
+	 * null ako je uneti String null.
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
@@ -25,7 +44,7 @@ public class VulkanizerskaRadnja {
 		for (int i = 0; i < gume.size(); i++)
 			if (gume.get(i).equals(markaModel))
 				novaLista.add(gume.get(i));
-		
+
 		return novaLista;
 	}
 }
